210be5c3b4f5c9b5fbed8b7e6dbf4dc8c6018c32
Kenn <162047143+HubertusKenneth@users.noreply.github.com>
2025-06-06 00:15:40 +0700

    Fixed primitive obsession,Long Method,Duplicate Code

--- Player.java
+++ Player.java
@@ -1,64 +1,55 @@
 import java.util.ArrayList; 
 
 public class Player{
-    private ArrayList<String> handColors = new ArrayList<String>();
-    private ArrayList<Integer> handNumbers = new ArrayList<Integer>();
-    private ArrayList<String> paletteColors = new ArrayList<String>();
-    private ArrayList<Integer> paletteNumbers = new ArrayList<Integer>();
+    private ArrayList<Card> hand = new ArrayList<>();
+    private ArrayList<Card> palette = new ArrayList<>();
 
 	public Player clonePlayer() {
         Player copy = new Player();
-		for(String color : this.handColors){
-			copy.handColors.add(color);
-		}
-		for(Integer number : this.handNumbers){
-			copy.handNumbers.add(number);
-		}
-		for(String color : this.paletteColors){
-			copy.paletteColors.add(color);
-		}
-		for(Integer number : this.paletteNumbers){
-			copy.paletteNumbers.add(number);
-		}
+        for (Card card : this.hand) {
+            copy.hand.add(new Card(card.getColor(), card.getNumber()));
+        }
+        for (Card card : this.palette) {
+            copy.palette.add(new Card(card.getColor(), card.getNumber()));
+        }
         return copy;
     }
 
-	public final ArrayList<String> getHandColors() {
-		return handColors;
-	}
-	
-	public final void setHandColors(ArrayList<String> handColors) {
-		this.handColors = handColors;
-	}
-	public final ArrayList<Integer> getHandNumbers() {
-		return handNumbers;
-	}
-	public final void setHandNumbers(ArrayList<Integer> handNumbers) {
-		this.handNumbers = handNumbers;
-	}
-	public final ArrayList<String> getPaletteColors() {
-		return paletteColors;
-	}
-	public final void setPaletteColors(ArrayList<String> paletteColors) {
-		this.paletteColors = paletteColors;
-	}
-	public final ArrayList<Integer> getPaletteNumbers() {
-		return paletteNumbers;
-	}
-	public final void setPaletteNumbers(ArrayList<Integer> paletteNumbers) {
-		this.paletteNumbers = paletteNumbers;
-	}
-
-	public void removeHandColors(int index) {
-		handColors.remove(index);
-	}
-	public void removeHandNumbers(int index) {
-		handNumbers.remove(index);
-	}
-	public void addPaletteColors(String color) {
-        paletteColors.add(color);
+	public static <T> void replaceContentsWithAnother(ArrayList<T> toReplace, ArrayList<T> newContents) {
+        toReplace.clear();
+        toReplace.addAll(newContents);
     }
-	public void addPaletteNumbers(int number) {
-        paletteNumbers.add(number);
+
+    public void copyFrom(Player other) {
+        replaceContentsWithAnother(this.hand, other.hand);
+        replaceContentsWithAnother(this.palette, other.palette);
+    }
+
+	public ArrayList<Card> getHand() {
+        return hand;
+    }
+
+    public ArrayList<Card> getPalette() {
+        return palette;
+    }
+
+	public void setHand(ArrayList<Card> hand) {
+        this.hand = new ArrayList<>(hand);
+    }
+
+	public void setPalette(ArrayList<Card> palette) {
+        this.palette = new ArrayList<>(palette);
+    }
+
+    public Card removeCardFromHand(int index) {
+        return hand.remove(index);
+    }
+
+    public void addCardToPalette(Card card) {
+        palette.add(card);
     }
 }
+
+
+
+	
