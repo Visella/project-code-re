e27b7dc55b307990f89a20f5e6481db06f160e20
Kenn <162047143+HubertusKenneth@users.noreply.github.com>
2025-06-06 00:16:30 +0700

    Fixed primitive obsession,Long Method,Duplicate Code

---
+++ CardLocation.java
@@ -0,0 +1,9 @@
+public class CardLocation {
+	final int colorIndex;
+    final int numberIndex;
+    
+    CardLocation(int colorIndex, int numberIndex) {
+        this.colorIndex = colorIndex;
+        this.numberIndex = numberIndex;
+    }
+}
